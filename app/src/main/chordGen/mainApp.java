64acfba7918f06b38489c38e8f2e5dee1a9938a0
Gekire156 <gavin.letulle@icloud.com>
2024-05-03 15:05:10 -0500

    2nd commit

--- app/src/main/chordGen/mainApp.java
+++ app/src/main/chordGen/mainApp.java
@@ -1,5 +1,8 @@
 package app.src.main.chordGen;
 
 public class mainApp{
-    
+    public static void main(String[] args){
+        System.out.println("Welcome to my Chord Progression Generator!");
+        
+    }
 }
